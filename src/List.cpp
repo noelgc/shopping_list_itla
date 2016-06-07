b0bcf88823778a95c7d8c41b12bc6a25c7450012
noel <noel>
2016-06-06 23:22:24 -0400

    probelama resuelto

--- src/List.cpp
+++ src/List.cpp
@@ -28,3 +28,53 @@ void List::remove(int index)
 	//TODO: Implement this method
 	cout << "You should write the code for removing the index " << index << endl;
 }
+
+
+
+
+#include <stdi.h>
+
+typedef struct {
+    struct nodo* siguiente;
+     char*  nobre;
+
+}  nodo;
+   nodo* primer = null;
+    nodo* ultimo = null;
+
+
+     void agregar ( nodo* _nodo){
+         _nodo=> suguiente =null
+      if(primer == null) {
+          primer = nodo;
+          ultimo = nodo;
+      } else{
+        ultimo => siguiente =_nodo;
+        ultimo = _nodo;
+      }
+
+
+     }
+
+      int main(){
+         nodo*primernodo=malloc(sizeof(nodo));
+         primernodo =>nombre = " primer elemento";
+         nodo*primernodo=malloc(sizeof(nodo));
+         segundonodo =>nombre = " segundo  elemento";
+           nodo*primernodo=malloc(sizeof(nodo));
+         tercernodo =>nombre = " tercer  elemento";
+
+         agregar(primernodo)
+         agregar(segundonodo)
+          agregar(tercernodo)
+
+           nodo = i = primernodo;
+           while(i!= null);{
+           printf("%s\n", i=>nombre );
+           i = i => siguiente
+           }
+
+      return 0;
+
+
+      }
